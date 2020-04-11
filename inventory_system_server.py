5f7cb7ce8e193ac508cae80334eb2306561d7b65
giraldoj@moravian.edu <giraldoj@moravian.edu>
2020-04-11 16:28:09 -0400

    I add the XML-RPC arg parse section

--- inventory_system_server.py
+++ inventory_system_server.py
@@ -12,17 +12,19 @@ import XMLRPC
 def main():
     try:
         if os.path.exists("products.pickle"):
-            products = open("products.pickle", "rb")
-            inventory = pickle.load(products)
+            try:
+                products = open("products.pickle", "rb")
+                inventory = pickle.load(products)
+            except EOFError:
+                inventory = Inventory()
         else:
             inventory = Inventory()
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
         inventory_system_pb2_grpc.add_InventorySystemServicer_to_server(InventorySystem(inventory), server)
         server.add_insecure_port('[::]:50051')
         server.start()
-        server_xml = server = SimpleXMLRPCServer(("localhost", 8000))
+        server_xml = SimpleXMLRPCServer(("localhost", 50051))
         XMLRPC.start_xml(server_xml, inventory)
-
         server.wait_for_termination()
     except KeyboardInterrupt:
         server_xml.shutdown()
